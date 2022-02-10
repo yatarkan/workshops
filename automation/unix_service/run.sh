a1d3c7573ba32771ac372ae5254cdef864530dca
Artyom Tugaryov <artyom.tugarev@intel.com>
2022-02-10 10:50:45 +0300

    Update run.sh

--- automation/unix_service/run.sh
+++ automation/unix_service/run.sh
@@ -7,7 +7,7 @@ USER_HOME_DIR=/home/workbench
 FULL_WORKSHOP_PATH=${USER_HOME_DIR}/.workbench/workshop
 
 rm -rf ${FULL_WORKSHOP_PATH}
-git clone https://github.com/dl-wb-experiments/face-hiding-workshop ${FULL_WORKSHOP_PATH}
+git clone https://github.com/dl-wb-experiments/workshops.git ${FULL_WORKSHOP_PATH}
 
 chmod -R 777 ${FULL_WORKSHOP_PATH}
 
